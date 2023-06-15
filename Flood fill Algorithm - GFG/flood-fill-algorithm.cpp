72e913a89a1fad715a1a4e8df0472a2814edfd34
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-06-15 16:14:08 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) 
+    {
+        // Code here 
+       int n = image.size();
+       int m = image[0].size();
+       int initcolor = image[sr][sc];
+       queue<pair<int,int>> q;
+       q.push({sr,sc});
+       
+       vector<vector<int>> color = image;
+       vector<vector<int>> vis(n,vector<int>(m,0));
+       vis[sr][sc] = 1;
+       color[sr][sc] = newColor;
+       int dr[] ={-1,0,+1,0};
+       int dc[] ={0,+1,0,-1};
+       while(!q.empty())
+       {
+           int row = q.front().first;
+           int col = q.front().second;
+           q.pop();
+            
+            for(int i = 0;i<4;i++)
+            {
+                int nrow = dr[i] + row;
+                int ncol = dc[i] + col;
+                if(nrow>=0 && nrow<n &&ncol>=0 && ncol<m && vis[nrow][ncol]==0 && color[nrow][ncol]==initcolor)
+                {
+                    vis[nrow][ncol] = 1;
+                    color[nrow][ncol] = newColor;
+                    q.push({nrow,ncol});
+                }
+            }
+       }
+       return color;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>image(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
