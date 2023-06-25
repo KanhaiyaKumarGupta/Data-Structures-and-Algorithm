841965383e6489f46f937fed77ed81eb0ab00758
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-06-25 14:53:08 +0530

    Added solution - LeetHub

---
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){ 
+        int cnt = 0;
+        for(int i = 0;i<n;i++)
+        {
+            string s = arr[i];
+            bool swapped = false;
+            int cn = 0;
+            for(int j = 0;j<k;j++)
+            {
+                if(s[j]==str[j])
+                {
+                    cn++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(cn==k)  
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        for(int i=0;i<n;i++)
+        {
+            string s;
+            cin>>arr[i];
+            
+        }
+        int k;
+        cin>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout << ob.klengthpref(arr, n, k, str) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
