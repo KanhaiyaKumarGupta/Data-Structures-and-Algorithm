f4049ad3d767abd842b95761a3073a0662635787
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-07-03 19:48:13 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int minDeletions(string str, int n);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cout << minDeletions(s, n) << endl;
+
+    }
+return 0;
+}
+// } Driver Code Ends
+
+int minDeletions(string s1, int n)
+{ 
+    //complete the function here 
+    string s2= s1;
+    reverse(s2.begin(),s2.end());
+    int dp[n+1][n+1];
+    for(int i = 0;i<n+1;i++)
+    {
+        dp[i][0] = 0;
+    }
+    for(int i = 0;i<n+1;i++)
+    {
+        dp[0][i] = 0;
+    }
+    for(int i = 1;i<n+1;i++)
+    {
+        for(int j = 1;j<n+1;j++)
+        {
+            if(s1[i-1]==s2[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return n - dp[n][n];
+} 
