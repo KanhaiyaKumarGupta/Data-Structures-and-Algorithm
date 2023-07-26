4983a2cc149ff2dd270616c6a861fc745eddab49
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-07-26 19:11:45 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -31,8 +31,35 @@ class Solution
         // Code here
         int m = s.size();
         int n = t.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return Distance(m,n,s,t,dp);
+        //vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        
+        for(int  i = 0;i<=m;i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int  i = 0;i<=n;i++)
+        {
+            dp[0][i] = i;
+        }
+        for(int i = 1;i<=m;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] =  1 + min(min(dp[i-1][j-1], dp[i-1][j]) , dp[i][j-1] );
+                }
+            }
+        }
+        return dp[m][n];
+        
+        
+       // return Distance(m,n,s,t,dp);
         
     }
 };
