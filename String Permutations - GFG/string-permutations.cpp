ab6b558627c8964121decf00f7665b8fd87af713
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-08-06 21:10:09 +0530

    Added solution - LeetHub

---
+++ String Permutations - GFG/string-permutations.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    vector<string> ans;
+   void generate(string s , int i)
+   {
+       if(i==s.size())
+       {
+           ans.push_back(s);
+       }
+       for(int j = i;j<s.size();j++)
+        {
+            swap(s[i],s[j]);
+            generate(s,i+1);
+            swap(s[i],s[j]);
+        }
+        return ;
+   }
+    vector<string> permutation(string S)
+    {
+         generate(S,0);
+         sort(ans.begin(),ans.end());
+         return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string S;
+		cin>>S;
+		Solution ob;
+		vector<string> vec = ob.permutation(S);
+		for(string s : vec){
+		    cout<<s<<" ";
+		}
+		cout<<endl;
+	
+	}
+	return 0;
+}
+// } Driver Code Ends
