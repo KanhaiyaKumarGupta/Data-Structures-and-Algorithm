c3f6153b5e124b66ebfc814178160570964a0720
KanhaiyaKumarGupta <120053612+KanhaiyaKumarGupta@users.noreply.github.com>
2023-08-10 21:59:17 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -13,26 +13,18 @@ class Solution
     int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        int dp[n+1][m+1];
-        for(int i = 0;i<n+1;i++)
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i = 1;i<=n;i++)
         {
-            dp[i][0] = 0;
-        }
-        for(int i = 0;i<m+1;i++)
-        {
-            dp[0][i] = 0;
-        }
-        for(int i =1;i<n+1;i++)
-        {
-            for(int j =1;j<m+1;j++)
+            for(int j =1;j<=m;j++)
             {
                 if(s1[i-1]==s2[j-1])
                 {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j]  =  1+ dp[i-1][j-1];
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
@@ -44,15 +36,15 @@ class Solution
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
